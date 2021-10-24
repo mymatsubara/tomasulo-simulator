4098812e66d645f59365e6517f47df47afd394c1
Murilo Matsubara <murilo.matsubara@gmail.com>
2021-10-24 17:49:08 -0300

    Implementação de exemplo no decode

--- decode.py
+++ decode.py
@@ -12,5 +12,16 @@ def decode(instruction, id):
     ### Implementação aqui ###
     ##########################
 
+    splitted = instruction.split(" ")
+    name = splitted[0]
+    # Exemplo de implementação do add (talvez tenha que alterar)
+    if (name == "add"):
+        # add 1, 2, 3
+        splitted = [s.strip(",") for s in splitted]
+        return Instruction(name, id, int(splitted[1]), int(splitted[2]), int(splitted[3]), "add")
+
+
     # Tudo que está entre [chaves] é para ser substituído
-    # return Instruction([name: string], id, [rd: int], [rs1: int], [rs2: int], [uf: "load"|"add"|"mult"])
+    # return Instruction("[name: string]", id, [rd: int], [rs1: int], [rs2: int], [uf: "load"|"add"|"mult"])
+    # return Instruction("lb", id, 1, 2, -1, "load")
+    # load 
